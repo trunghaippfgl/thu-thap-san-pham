67e33c9f9a7f6b0ae0c257d895bfb54b7d7f4048
trunghaippfgl <trunghaippfgl@gmail.com>
2026-09-17 21:29:13 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='collector-cloud-v02';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
